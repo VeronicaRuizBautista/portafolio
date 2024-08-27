667d0c56e2ebfe547decdf9847e11f697fb7a486
VeronicaRuizBautista <156429590+VeronicaRuizBautista@users.noreply.github.com>
2024-08-27 00:10:43 -0500

    feat: :sparkles: Diseño linea de tiempo y mas
    Se agrego la linea de tiempo, una img a la seccion sobre mi y transiciones a los proyectos

--- js/main.js
+++ js/main.js
@@ -6,3 +6,38 @@ window.addEventListener('scroll', () => {
     div.classList.remove('visible');
   }
 });
+
+window.addEventListener('scroll', () => {
+  const proyectos = document.querySelectorAll('.proyecto');
+
+  proyectos.forEach(proyecto => {
+    if (window.scrollY > 1000) { // Ajusta el punto de desplazamiento
+      proyecto.classList.add('visible');
+    } else {
+      proyecto.classList.remove('visible');
+    }
+  });
+});
+
+const circle = document.querySelector('.circle');
+const events = document.querySelectorAll('.event');
+const timelineHeight = document.querySelector('.timeline').clientHeight; // Obtener la altura de la línea de tiempo
+const eventHeight = 150
+
+let currentEvent = 1;
+
+function animateCircle() {
+  circle.style.top = currentEvent * eventHeight + 'px';
+  events[currentEvent].style.opacity = 1;
+
+  if (currentEvent < events.length - 1) {
+    currentEvent++;
+    setTimeout(animateCircle, 1000);
+  } else {
+    // Reiniciar la animación si es necesario
+    currentEvent = 0;
+    setTimeout(animateCircle, 1000);
+  }
+}
+
+animateCircle();
